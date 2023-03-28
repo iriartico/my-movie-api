4ef1426efe9b69f184412e36b570a3b89a435399
Steve Jobs <sexyespadachin1@gmail.com>
2023-03-28 14:52:25 -0400

    Route parameters

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ movies = [
         'year': '2009',
         'rating': 7.8,
         'category': 'Acción'
+    },
+    {
+        'id': 2,
+        'title': 'Avatar 2',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, cotinuan con ...",
+        'year': '2022',
+        'rating': 7.1,
+        'category': 'Acción'
     }
 ]
 
@@ -26,3 +34,9 @@ def home():
 @app.get('/movies', tags=['movies'])
 def get_movies():
     return movies
+
+
+@app.get('/movies/{id}', tags=['movies'])
+def get_movie(id: int):
+    movie = list(filter(lambda x: x['id'] == id, movies))
+    return movie[0] if len(movie) > 0 else "Nothing to show"
