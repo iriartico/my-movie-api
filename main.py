5a4844554c52fde8ebb0a5059b22ab758a24c855
Steve Jobs <sexyespadachin1@gmail.com>
2023-03-28 12:20:29 -0400

    GET Method

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+app.title = "My app with FastAPI"
+app.version = "0.0.1"
 
 
-@app.get('/')
-def message():
-    return ('Hello World!')
+movies = [
+    {
+        'id': 1,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'
+    }
+]
+
+
+@app.get('/', tags=['home'])
+def home():
+    return HTMLResponse('<h1>Home Page</h1>')
+
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
