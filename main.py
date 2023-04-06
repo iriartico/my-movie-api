48cdbc887cd28a7442f2429e1068d0520059ab43
Steve Jobs <sexyespadachin1@gmail.com>
2023-04-06 15:13:44 -0400

    Creation of models

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from pydantic import BaseModel, Field
 from typing import Optional
 from jwt_manager import create_token, validate_token
 from fastapi.security import HTTPBearer
+from config.database import Session, engine, Base
+from models.movie import Movie
 
 
 app = FastAPI()
 app.title = "My app with FastAPI"
 app.version = "0.0.1"
+
+Base.metadata.create_all(bind=engine)
 
 
 class JWTBearer(HTTPBearer):
