53ada66b1ca043c7e1bfb4c2fbd06014b871d087
Steve Jobs <sexyespadachin1@gmail.com>
2023-04-06 09:23:06 -0400

    Validating tokens

--- main.py
+++ main.py
@@ -64,7 +64,9 @@ def home():
 
 @app.post('/login', tags=['auth'])
 def login(user: User):
-    return user
+    if user.email == "admin@email.com" and user.password == "admin123":
+        token: str = create_token(user.dict())
+    return JSONResponse(status_code=200, content=token)
 
 
 @app.get('/movies', tags=['movies'], status_code=200)
